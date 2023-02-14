ddf51be274d7ee85ca32081bd63a485a3a0efa92
D3L1J3 <37899526+D3L1J3@users.noreply.github.com>
2023-02-14 14:36:40 +0100

    update test

--- cypress/e2e/automation-test-store/contact-us.js
+++ cypress/e2e/automation-test-store/contact-us.js
@@ -1,13 +1,18 @@
 /// <reference types="cypress" />
 
 describe("Test Contact Us form via Automation Test Store", () => {
+    before(() => {
+        cy.fixture("userDetails").as("user");
+    });
     it("Should be able to submit a successful submission via contact us form", () => {
         cy.visit('https://www.automationteststore.com/');
         cy.get("a[href$='contact']").click().then(function(linkText) {
             cy.log("Clicked on link using text: " + linkText.text());
         });
-        cy.get('#ContactUsFrm_first_name').type("Joe");
-        cy.get('#ContactUsFrm_email').type("joe_blogs123@gmail.com");
+        cy.get("@user").then((user) => {
+            cy.get('#ContactUsFrm_first_name').type(user.first_name);
+            cy.get('#ContactUsFrm_email').type(user.email);
+        })
         cy.get('#ContactUsFrm_email').should('have.attr', 'name', 'email');
         cy.get('#ContactUsFrm_enquiry').type("Do you provide additional discount on bulk orders?");
         cy.get('button[title="Submit"]').click();

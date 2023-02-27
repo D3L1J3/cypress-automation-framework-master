bf6e6902b16ac50766f53eb4b9de94b1febc225e
D3L1J3 <37899526+D3L1J3@users.noreply.github.com>
2023-02-27 09:57:06 +0100

    update json examples

--- cypress/e2e/other/json-examples.js
+++ cypress/e2e/other/json-examples.js
@@ -4,6 +4,17 @@ describe("JSON Object", () => {
     it("Json Object Examples", () => {
         const exampleObject = {"key": "Dragan", "key2": "Maksimovic"};
         const exampleArrayOfValues = ["Sophie", "Rose", "Howard"];
+        const exampleArrayOfObjects = [
+            {
+                "key": "Luke",
+            },
+            {
+                "key2": "Skywalker"
+            },
+            {
+                "key3": 22
+            },
+        ]
 
         const users = {
             "firstName": "Joe", 
@@ -29,6 +40,9 @@ describe("JSON Object", () => {
         cy.log(exampleArrayOfValues[1]); // Rose
 
         cy.log(users.Students[0].lastName); // Blogs2
+        cy.log(exampleArrayOfObjects[0].key); // Luke
+        cy.log(exampleArrayOfObjects[1].key2); // Skywalker
+        cy.log(exampleArrayOfObjects[2].key3); // 22
     });
 })
 
